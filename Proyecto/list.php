0351aefb018179131ce423f98c96accb54dfc507
fabricio zavala <fabriciozavala13@gmail.com>
2023-11-25 11:43:26 -0500

    cambio

--- Proyecto/list.php
+++
@@ -1,7 +0,0 @@
-<?php
-$documentList = array(); // Aquí deberías consultar la base de datos para obtener la lista de documentos
-
-// Código para consultar la base de datos y llenar $documentList
-
-echo json_encode($documentList);
-?>
